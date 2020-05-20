41da559ccf033b8c5ba037e3c0d480d8c1419a9f
0x113 <jose.guombinho@gmail.com>
2020-05-20 15:07:14 +0000

    custom client Do func

--- mocks/client.go
+++ mocks/client.go
@@ -8,9 +8,13 @@ type MockClient struct {
 	ExpectedRes *http.Response
 	Req         *http.Request
 	Err         error
+	DoFunc      func(req *http.Request) (*http.Response, error)
 }
 
 func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
 	m.Req = req
+	if m.DoFunc != nil {
+		return m.DoFunc(req)
+	}
 	return m.ExpectedRes, m.Err
 }
