5bc8c98cf67b57668cdc27e33c6b7536e2c6d7cd
0x113 <jose.guombinho@gmail.com>
2020-05-25 14:47:52 +0000

    added school test

---
+++ school_test.go
@@ -0,0 +1,59 @@
+package librus_api_go_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	golibrus "github.com/0x113/librus-api-go"
+	"github.com/0x113/librus-api-go/mocks"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSuccessGetSchoolInfo(t *testing.T) {
+	client := &mocks.MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			json := `{"School": {"Name": "Cool school", "Town": "Cracow", "Street": "Sezamkowa", "State": "-", "BuildingNumber": "12"}}`
+			return &http.Response{
+				Body: ioutil.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	l := &golibrus.Librus{Client: client}
+
+	// set authorization token
+	golibrus.Headers[0].Value = "Bearer HESOYAM"
+
+	expectedSchool := &golibrus.School{
+		Name:           "Cool school",
+		Town:           "Cracow",
+		Street:         "Sezamkowa",
+		State:          "-",
+		BuildingNumber: "12",
+	}
+
+	schoolInfo, err := l.GetSchoolInfo()
+	assert.Nil(t, err)
+	assert.Equal(t, expectedSchool, schoolInfo)
+}
+
+func TestFailGetSchoolInfo(t *testing.T) {
+	client := &mocks.MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			json := `{"School": {"Name": "Cool school", "Town": "Cracow", "Street": "Sezamkowa", "State": "-", "BuildingNumber": 12}}` // invalid json (BuildingNumber should be string not int)
+			return &http.Response{
+				Body: ioutil.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	l := &golibrus.Librus{Client: client}
+
+	// set authorization token
+	golibrus.Headers[0].Value = "Bearer HESOYAM"
+
+	schoolInfo, err := l.GetSchoolInfo()
+	assert.NotNil(t, err)
+	assert.Nil(t, schoolInfo)
+}
