cc53bfeaba59c160261878c543389d42d86ef166
0x113 <jose.guombinho@gmail.com>
2020-05-25 20:00:33 +0000

    tests

---
+++ subject_test.go
@@ -0,0 +1,53 @@
+package librus_api_go_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	golibrus "github.com/0x113/librus-api-go"
+
+	"github.com/0x113/librus-api-go/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSuccessGetSubject(t *testing.T) {
+	client := &mocks.MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			json := `{"Subject": {"Name": "Math"}}`
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	l := &golibrus.Librus{Client: client}
+
+	// set authorization token
+	golibrus.Headers[0].Value = "Bearer HESOYAM"
+
+	subject, err := l.GetSubject(123)
+	assert.Nil(t, err)
+	assert.Equal(t, subject, "Math")
+}
+
+func TestFailGetSubject(t *testing.T) {
+	client := &mocks.MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			json := `{"Subject": {"SubjectName": "Math"}}` // invalid json (SubjectName should be nammed Name)
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	l := &golibrus.Librus{Client: client}
+
+	// set authorization token
+	golibrus.Headers[0].Value = "Bearer HESOYAM"
+
+	subject, err := l.GetSubject(123)
+	assert.Nil(t, err)
+	assert.Empty(t, subject)
+}
