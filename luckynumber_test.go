c36a395074d682dcace787daad768bd72c5b1646
0x113 <jose.guombinho@gmail.com>
2020-05-25 08:56:30 +0000

    lucky number tests

---
+++ luckynumber_test.go
@@ -0,0 +1,54 @@
+package librus_api_go_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	golibrus "github.com/0x113/librus-api-go"
+	"github.com/0x113/librus-api-go/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSuccessGetLuckyNumber(t *testing.T) {
+	client := &mocks.MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			json := `{"LuckyNumber": {"LuckyNumber": 1, "LuckyNumberDay": "2020-01-03"}}`
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	l := &golibrus.Librus{Client: client}
+
+	// set authorization token
+	golibrus.Headers[0].Value = "Bearer HESOYAM"
+
+	luckyNumber, err := l.GetLuckyNumber()
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, luckyNumber.LuckyNumber)
+	assert.Equal(t, "2020-01-03", luckyNumber.LuckyNumberDay)
+}
+
+func TestFailGetLuckyNumber(t *testing.T) {
+	client := &mocks.MockClient{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			json := `{"LuckyNumber": "1", "LuckyNumberDay": "2029-01-93"}` // invalid json
+			return &http.Response{
+				StatusCode: http.StatusInternalServerError,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+			}, nil
+		},
+	}
+	l := &golibrus.Librus{Client: client}
+
+	// set authorization token
+	golibrus.Headers[0].Value = "Bearer HESOYAM"
+
+	luckyNumber, err := l.GetLuckyNumber()
+	assert.NotNil(t, err)
+	assert.Nil(t, luckyNumber)
+}
